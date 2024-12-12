7847b2c3322405bd09790592ec165d0c80287b35
George georgiou <georgiougt94@icloud.com>
2024-12-12 16:24:42 +0200

    second

---
+++ dog.go
@@ -0,0 +1,6 @@
+package dog
+
+func WhenGrowUp(s string) string {
+
+	return "When the puppy grows up it says : " + strings.toUpper(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/georgiougt/dog
+
+go 1.23.2
